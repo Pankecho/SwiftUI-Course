a11761a40f6e5d8bc62ed535f9c42bd60a1949b3
Juan Pablo <pankecho@JOKASTA.local>
2022-02-19 01:32:55 -0600

    Finish Chapter 14 Combine and EnvironmentObject

--- SwiftUI Course/FormExample.swift
+++ SwiftUI Course/FormExample.swift
@@ -5,6 +5,7 @@
 //  Created by Juan Pablo Martinez Ruiz on 18/02/22.
 //
 
+import Combine
 import SwiftUI
 
 struct DinnerListView: View {
@@ -35,47 +36,51 @@ struct DinnerListView: View {
     @State private var selectedDinner: Dinner?
     @State private var showSettings: Bool = false
 
+    @EnvironmentObject var settingStore: SettingsStore
+
     var body: some View {
         NavigationView {
             List {
-                ForEach(dinners) { dinner in
-                    BasicImageRow(dinner: dinner)
-                        .contextMenu {
+                ForEach(dinners.sorted(by: DisplayOrderType(type: settingStore.displayOrder).predicate())) { dinner in
+                    if shouldShowItem(dinner: dinner) {
+                        BasicImageRow(dinner: dinner)
+                            .contextMenu {
 
-                            Button(action: {
-                                // mark the selected restaurant as check-in
-                                self.checkIn(with: dinner.id)
-                            }) {
-                                HStack {
-                                    Text("Check-in")
-                                    Image(systemName: "checkmark.seal.fill")
+                                Button(action: {
+                                    // mark the selected restaurant as check-in
+                                    self.checkIn(with: dinner.id)
+                                }) {
+                                    HStack {
+                                        Text("Check-in")
+                                        Image(systemName: "checkmark.seal.fill")
+                                    }
+                                }
+
+                                Button(action: {
+                                    // delete the selected restaurant
+                                    self.delete(with: dinner.id)
+                                }) {
+                                    HStack {
+                                        Text("Delete")
+                                        Image(systemName: "trash")
+                                    }
+                                }
+
+                                Button(action: {
+                                    // mark the selected restaurant as favorite
+                                    self.setFavorite(with: dinner.id)
+
+                                }) {
+                                    HStack {
+                                        Text("Favorite")
+                                        Image(systemName: "star")
+                                    }
                                 }
                             }
-
-                            Button(action: {
-                                // delete the selected restaurant
-                                self.delete(with: dinner.id)
-                            }) {
-                                HStack {
-                                    Text("Delete")
-                                    Image(systemName: "trash")
-                                }
+                            .onTapGesture {
+                                self.selectedDinner = dinner
                             }
-
-                            Button(action: {
-                                // mark the selected restaurant as favorite
-                                self.setFavorite(with: dinner.id)
-
-                            }) {
-                                HStack {
-                                    Text("Favorite")
-                                    Image(systemName: "star")
-                                }
-                            }
-                        }
-                        .onTapGesture {
-                            self.selectedDinner = dinner
-                        }
+                    }
                 }
                 .onDelete { (indexSet) in
                     self.dinners.remove(atOffsets: indexSet)
@@ -92,7 +97,7 @@ struct DinnerListView: View {
                 }
             }
             .sheet(isPresented: $showSettings) {
-                FormExample()
+                FormExample().environmentObject(settingStore)
             }
             .navigationViewStyle(.stack)
         }
@@ -117,21 +122,21 @@ struct DinnerListView: View {
             self.dinners[index].isCheckIn.toggle()
         }
     }
+
+    private func shouldShowItem(dinner: Dinner) -> Bool {
+        return (!settingStore.showCheckInOnly || dinner.isCheckIn) && (dinner.priceLevel <= settingStore.maxPriceLevel)
+    }
 }
 
 
 struct FormExample: View {
     @Environment(\.presentationMode) var mode
-    
-    private let displayOrder = [
-        "Alphabetical",
-        "Show Favorite First",
-        "Show Check-in First"
-    ]
 
-    @State private var selectedOrder = 0
+    @State private var selectedOrder: DisplayOrderType = .alphabetical
     @State private var showCheckInOnly = false
     @State private var maxPriceLevel = 5
+
+    @EnvironmentObject var settingStore: SettingsStore
 
     var body: some View {
         NavigationView {
@@ -139,8 +144,8 @@ struct FormExample: View {
                 Section(header: Text("SORT PREFERENCE")) {
                     Picker(selection: $selectedOrder,
                            label: Text("Display Order")) {
-                        ForEach(0 ..< displayOrder.count, id: \.self) {
-                            Text(displayOrder[$0])
+                        ForEach(DisplayOrderType.allCases, id: \.self) { order in
+                            Text(order.text)
                         }
                     }
 
@@ -179,18 +184,26 @@ struct FormExample: View {
                     }
                             , trailing:
                     Button {
+                        settingStore.showCheckInOnly = showCheckInOnly
+                        settingStore.displayOrder = selectedOrder.rawValue
+                        settingStore.maxPriceLevel = maxPriceLevel
                         mode.wrappedValue.dismiss()
                     } label: {
                         Text("Apply")
                     }
             )
+            .onAppear {
+                selectedOrder = DisplayOrderType(type: settingStore.displayOrder)
+                showCheckInOnly = settingStore.showCheckInOnly
+                maxPriceLevel = settingStore.maxPriceLevel
+            }
         }
     }
 }
 
 struct FormExample_Previews: PreviewProvider {
     static var previews: some View {
-        DinnerListView()
+        DinnerListView().environmentObject(SettingsStore())
     }
 }
 
@@ -258,4 +271,61 @@ struct BasicImageRow: View {
     }
 }
 
+enum DisplayOrderType: Int, CaseIterable {
+    case alphabetical = 0
+    case favoriteFirst = 1
+    case checkInFirst = 2
 
+    var text: String {
+        switch self {
+        case .alphabetical: return "Alphabetical"
+        case .favoriteFirst: return "Show Favorite First"
+        case .checkInFirst: return "Show Check-in First"
+        }
+    }
+
+    init(type: Int) {
+        switch type {
+        case 0: self = .alphabetical
+        case 1: self = .favoriteFirst
+        case 2: self = .checkInFirst
+        default: self = .alphabetical
+        }
+    }
+
+    func predicate() -> ((Dinner, Dinner) -> Bool) {
+        switch self {
+        case .alphabetical: return { $0.name < $1.name }
+        case .favoriteFirst: return { $0.isCheckIn && !$1.isFavorite }
+        case .checkInFirst: return { $0.isCheckIn && !$1.isCheckIn }
+        }
+    }
+}
+
+final class SettingsStore: ObservableObject {
+    @Published var showCheckInOnly: Bool = UserDefaults.standard.bool(forKey: "view.preferences.showCheckInOnly") {
+        didSet {
+            UserDefaults.standard.set(showCheckInOnly, forKey: "view.preferences.showCheckInOnly")
+        }
+    }
+
+    @Published var displayOrder: Int = UserDefaults.standard.integer(forKey: "view.preferences.displayOrder") {
+        didSet {
+            UserDefaults.standard.set(displayOrder, forKey: "view.preferences.displayOrder")
+        }
+    }
+
+    @Published var maxPriceLevel: Int = UserDefaults.standard.integer(forKey: "view.preferences.maxPriceLevel") {
+        didSet {
+            UserDefaults.standard.set(maxPriceLevel, forKey: "view.preferences.maxPriceLevel")
+        }
+    }
+
+    init() {
+        UserDefaults.standard.register(defaults: [
+            "view.preferences.showCheckInOnly": false,
+            "view.preferences.displayOrder": 0,
+            "view.preferences.maxPriceLevel": 5
+        ])
+    }
+}
